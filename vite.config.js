a011739daa6b0f9d727afcb92150d753dac49141
Frank Jimenez <frankjb79@gmail.com>
2025-06-09 18:58:17 +0200

    Arregladas rutas para el deploy en github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/proyecto_final_css/",
+  build: {
+    rollupOptions: {
+      input: [
+        "index.html",
+        "ingles.html",
+        "conocenos.html",
+        "blog.html",
+        "article-poder-tecnologia.html",
+        "article-extranjero.html",
+        "article-cinco-palabras.html",
+        "article-cinco-consejos.html",
+      ],
+    },
+  },
   server: {
     watch: {
-      usePolling: true
-    }
-  }
-})
+      usePolling: true,
+    },
+  },
+});
